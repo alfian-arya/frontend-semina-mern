e39d618d1699cd29f263d7d4061f2e03803c4ed2
alfian-arya <you@example.com>
2023-04-30 19:53:02 +0800

    update categories page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageSignin from "./pages/signin";
 import DashboardPage from "./pages/dashboard";
 import CategoriesPage from "./pages/categories";
+import CategoryCreate from "./pages/categories/create";
+import CategoryEdit from "./pages/categories/edit";
+// import CategoriesForm from "./pages/categories/form";
+
+
+
 
 function App() {
   return (
@@ -13,6 +19,10 @@ function App() {
           <Route path="/" element={<DashboardPage />} />
           <Route path="/signin" element={<PageSignin />} />
           <Route path="/categories" element={<CategoriesPage />} />
+          <Route path="/categories/create" element={<CategoryCreate />} />
+          <Route path="/categories/edit/:id" element={<CategoryEdit />} />
+
+
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -1,12 +1,36 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { Container, Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Container, Table, Spinner } from "react-bootstrap";
 import SButton from "../../components/Button";
 import SBreadCrumb from "../../components/BreadCrumb";
 import SNavbar from "../../components/Navbar";
+import axios from "axios";
+import { config } from "../../configs";
 
 function CategoriesPage() {
   const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const getCategoriesAPI = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios.get(`${config.api_host_dev}/cms/categories`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setIsLoading(false);
+        setData(res.data.data);
+      } catch (err) {
+        setIsLoading(false);
+        console.log(err);
+      }
+    };
+    getCategoriesAPI();
+  }, []);
 
   if (!token) return <Navigate to="/signin" replace={true} />;
   return (
@@ -14,34 +38,33 @@ function CategoriesPage() {
       <SNavbar />
       <Container className="mt-3">
         <SBreadCrumb textSecond="Categories" />
-        <SButton>Tambah</SButton>
+        <SButton action={() => navigate("/categories/create")}>Tambah</SButton>
         <Table className="mt-3" striped bordered hover variant="dark">
           <thead>
             <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>No</th>
+              <th>Name</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {isLoading ? (
+              <tr>
+                <td colSpan={data.length + 1} style={{ textAlign: "center" }}>
+                  <div className="flex items-center justify-center">
+                    <Spinner animation="grow" variant="light" />
+                  </div>
+                </td>
+              </tr>
+            ) : (
+              data.map((data, index) => (
+                <tr key={index}>
+                  <td>{(index += 1)}</td>
+                  <td>{data.name}</td>
+                  <td>Otto</td>
+                </tr>
+              ))
+            )}
           </tbody>
         </Table>
       </Container>
